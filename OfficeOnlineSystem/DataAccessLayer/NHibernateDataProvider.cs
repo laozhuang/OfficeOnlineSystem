c090a3a1b9723ba829334cd6c3ad1af59cf8793b
chenshuhui <a82703855@126.com>
2012-05-29 13:50:38 +0800

    Signed-off-by: chenshuhui <a82703855@126.com>

---
+++ OfficeOnlineSystem/DataAccessLayer/NHibernateDataProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate;
+using DataTransfer;
+
+namespace DataAccessLayer
+{
+    public class NHibernateDataProvider
+    {
+        protected ISession _session { get; set; }
+
+        public NHibernateDataProvider(ISession session)
+        {
+            _session = session;
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+
+            ISessionFactory _sessionFactory = (new Configuration()).Configure().BuildSessionFactory();
+
+            _session = _sessionFactory.OpenSession();
+            //session.Close();
+            return _session.Get<Customer>(customerId);
+            
+        }
+
+
+        public void CreateCustomer(Customer tempCutomer)
+        {
+            _session.Save(tempCutomer);
+            _session.Flush();
+        }
+
+
+        public void CreateUser(User tempUser)
+        {
+            _session.Save(tempUser);
+            _session.Flush();
+        }
+
+        public void CreateFile(Post_file postfile)
+        {
+            _session.Save(postfile);
+            _session.Flush();
+        }
+        public void CreateTreatFile(Treat_file treatfile)
+        {
+            _session.Save(treatfile);
+            _session.Flush();
+        }
+    }
+}
